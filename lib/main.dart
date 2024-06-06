17068fafe26d70dcecf3f2227c97e28e8bab142b
rafy <juvwind@gmail.com>
2024-06-06 22:15:14 +0900

    과제 초안 완성

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:onboarding_flow/features/authentication/confirmation_code_screen.dart';
+import 'package:onboarding_flow/features/authentication/customize_experience_screen.dart';
 import 'package:onboarding_flow/features/authentication/initial_screen.dart';
+import 'package:onboarding_flow/features/authentication/interests_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,11 +17,53 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          surfaceTintColor: Colors.white,
+        ),
+        primaryColor: Colors.blue[400],
+        scaffoldBackgroundColor: Colors.white,
         useMaterial3: true,
+        textTheme: TextTheme(
+          displayLarge: GoogleFonts.openSans(
+              fontSize: 96, fontWeight: FontWeight.w300, letterSpacing: -1.5),
+          displayMedium: GoogleFonts.openSans(
+              fontSize: 60, fontWeight: FontWeight.w300, letterSpacing: -0.5),
+          displaySmall:
+              GoogleFonts.openSans(fontSize: 48, fontWeight: FontWeight.w400),
+          headlineMedium: GoogleFonts.openSans(
+              fontSize: 34, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+          headlineSmall:
+              GoogleFonts.openSans(fontSize: 28, fontWeight: FontWeight.w800),
+          titleLarge: GoogleFonts.openSans(
+              fontSize: 20, fontWeight: FontWeight.w800, letterSpacing: 0.15),
+          titleMedium: GoogleFonts.openSans(
+              fontSize: 18, fontWeight: FontWeight.w600, letterSpacing: 0.15),
+          titleSmall: GoogleFonts.openSans(
+              fontSize: 15, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+          bodyLarge: GoogleFonts.roboto(
+              fontSize: 16, fontWeight: FontWeight.w800, letterSpacing: 0.5),
+          bodyMedium: GoogleFonts.roboto(
+              fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+          labelLarge: GoogleFonts.roboto(
+              fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+          bodySmall: GoogleFonts.roboto(
+              fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+          labelSmall: GoogleFonts.roboto(
+              fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
+        ),
+        bottomAppBarTheme: const BottomAppBarTheme(
+          color: Colors.white,
+          surfaceTintColor: Colors.white,
+          elevation: 1.0,
+          // color: Colors.white,
+        ),
       ),
       home: const InitialScreen(),
+      // home: const InterestsScreen(),
     );
   }
 }
